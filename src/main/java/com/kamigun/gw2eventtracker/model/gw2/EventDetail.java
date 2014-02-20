a05eb6a712c6b6b34d1d17b777c9eedbfac30d65
shinobi <shinobi@shinobi-PC>
2014-02-20 15:54:21 +0100

    added setters and getters

--- src/main/java/com/kamigun/gw2eventtracker/model/gw2/EventDetail.java
+++ src/main/java/com/kamigun/gw2eventtracker/model/gw2/EventDetail.java
@@ -12,10 +12,51 @@ import java.util.ArrayList;
  * @author shinobi
  */
 public class EventDetail {
+
     private String name;
     private Integer level;
     @JsonProperty(value = "map_id")
     private Integer mapId;
     private ArrayList<String> flags;
     private Location location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getMapId() {
+        return mapId;
+    }
+
+    public void setMapId(Integer mapId) {
+        this.mapId = mapId;
+    }
+
+    public ArrayList<String> getFlags() {
+        return flags;
+    }
+
+    public void setFlags(ArrayList<String> flags) {
+        this.flags = flags;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
