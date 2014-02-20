35ffeda1f1e5b2187265132982a156554dd26847
shinobi <shinobi@shinobi-PC>
2014-02-20 15:59:56 +0100

    added index view controller fixed httpClient settings

--- src/main/java/com/kamigun/gw2eventtracker/config/ApplicationConfig.java
+++ src/main/java/com/kamigun/gw2eventtracker/config/ApplicationConfig.java
@@ -5,13 +5,19 @@
 package com.kamigun.gw2eventtracker.config;
 
 import com.kamigun.gw2eventtracker.model.gw2.Names;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.http.impl.conn.SchemeRegistryFactory;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.SyncBasicHttpParams;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -23,8 +29,12 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.kamigun.gw2eventtracker"})
 public class ApplicationConfig extends WebMvcConfigurerAdapter {
-    private Log logger = LogFactory.getLog(getClass());
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("index.htm").setViewName("index");
+    }
+    
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -32,12 +42,27 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
-
+    
     @Bean
     public DefaultHttpClient httpClient() {
-        return new DefaultHttpClient();
-    }
+        HttpParams params = new SyncBasicHttpParams();
+        HttpConnectionParams.setSocketBufferSize(params, 16384);
+        HttpConnectionParams.setConnectionTimeout(params, 60000);
+        HttpConnectionParams.setSoTimeout(params, 60000);
 
+        SchemeRegistry schemeRegistry = SchemeRegistryFactory.createDefault();
+
+        PoolingClientConnectionManager connectionManager = new PoolingClientConnectionManager(schemeRegistry);
+        connectionManager.setDefaultMaxPerRoute(1000);
+        connectionManager.setMaxTotal(1000);
+
+        DefaultHttpClient client = new DefaultHttpClient(connectionManager, params);
+
+        client.setHttpRequestRetryHandler(new DefaultHttpRequestRetryHandler(3, true));
+
+        return client;
+    }
+    
     @Bean
     public Names names() {
         return new Names();
